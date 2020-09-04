1f91db4db5d665099761c840c20682015b06d9d6
IngridTPires <ingrid.teixeirap@gmail.com>
2020-09-03 23:51:00 -0300

    salvar chama post para json

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,33 @@
 <template lang="html">
-    <div id="app">
-        <Conteudo
-            :conteudo="conteudo"
-            @salvar="salvarConteudo" />
-    </div>
+  <div id="app">
+    <Conteudo :conteudo="conteudo" @salvar="salvarConteudo" />
+  </div>
 </template>
 
 <script>
-    import Conteudo from "./components/Conteudo.vue";
-    import "bootstrap/dist/css/bootstrap.min.css";
+import Conteudo from "./components/Conteudo.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-    export default {
-        name: 'App',
-        data () {
-            return {
-                conteudo: undefined
-            }
-        },
-        components: {
-            Conteudo,
-        },
-        methods: {
-            salvarConteudo (value) {
-                alert(`Evento para salvar ${value.titulo}`)
-            }
-        }
+export default {
+  name: "App",
+  data() {
+    return {
+      conteudo: undefined,
     };
+  },
+  components: {
+    Conteudo,
+  },
+  methods: {
+    salvarConteudo(value) {
+      alert(`Evento para salvar ${value.titulo}`);
+
+      this.$http
+        .post("http://localhost:3000/conteudos", value)
+        .then((res) => res.json());
+    },
+  },
+};
 </script>
 
 <style></style>
--- src/components/Conteudo.vue
+++ src/components/Conteudo.vue
@@ -1,55 +1,75 @@
 <template lang="html">
-    <div class="container">
-        <form @submit="salvar()">
-            <h2>Registro de conteúdo</h2>
-            <br />
-            <div class="form-group row">
-                <label class="col-sm-2 ">Título:</label>
-                <div class="col-sm-10"><input type="text" class="form-control" v-model="conteudoAtual.titulo" /></div>
-            </div>
-            <div class="form-group row">
-                <label class="col-sm-2 ">Autor:</label>
-                <div class="col-sm-10"><input type="text" class="form-control" v-model="conteudoAtual.autor" /></div>
-            </div>
-            <div class="form-group row">
-                <label class="col-sm-2 ">Assunto:</label>
-                <div class="col-sm-10"><input type="text" class="form-control" v-model="conteudoAtual.assunto" /></div>
-            </div>
-            <div class="form-group row">
-                <label class="col-sm-2 ">URL:</label>
-                <div class="col-sm-10"><input type="text" class="form-control" v-model="conteudoAtual.url" /></div>
-            </div>
-            <button type="submit" class="btn btn-primary float-right">Salvar</button>
-        </form>
-    </div>
+  <div class="container">
+    <form @submit="salvar()">
+      <h2>Registro de conteúdo</h2>
+      <br />
+      <div class="form-group row">
+        <label class="col-sm-2 ">Título:</label>
+        <div class="col-sm-10">
+          <input
+            type="text"
+            class="form-control"
+            v-model="conteudoAtual.titulo"
+          />
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 ">Autor:</label>
+        <div class="col-sm-10">
+          <input
+            type="text"
+            class="form-control"
+            v-model="conteudoAtual.autor"
+          />
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 ">Assunto:</label>
+        <div class="col-sm-10">
+          <input
+            type="text"
+            class="form-control"
+            v-model="conteudoAtual.assunto"
+          />
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-2 ">URL:</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" v-model="conteudoAtual.url" />
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary float-right">Salvar</button>
+    </form>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            conteudo: {
-                titulo: '',
-                autor: '',
-                assunto: '',
-                url: ''
-            }
-        },
-        data () {
-            return {
-                conteudoAtual: { ...this.conteudo }
-            }
-        },
-        methods: {
-            salvar () {
-                this.$emit('salvar', this.conteudoAtual);
-            }
-        }
+export default {
+  props: {
+    conteudo: {
+      titulo: "",
+      autor: "",
+      assunto: "",
+      url: "",
+    },
+  },
+  data() {
+    return {
+      conteudoAtual: { ...this.conteudo },
     };
+  },
+  methods: {
+    salvar() {
+      this.$emit("salvar", this.conteudoAtual);
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .container {
-        width: 40%;
-        margin: 20px auto 0px auto;
-    }
+.container {
+  width: 40%;
+  margin: 20px auto 0px auto;
+}
 </style>
