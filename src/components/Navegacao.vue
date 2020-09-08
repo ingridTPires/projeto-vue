62b654192634046b1ed2899f6fcfb61acdb2257d
IngridTPires <ingrid.teixeirap@gmail.com>
2020-09-07 22:26:00 -0300

    Barra de navegação

---
+++ src/components/Navegacao.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="barraNav">
+        <nav>
+            <a href="">Conteúdo</a>
+            <a href="">Relacionados</a>
+        </nav>
+    </div>
+</template>
+
+<style scoped>
+    .barraNav {
+        background-color: #333;
+    }
+    nav {
+        padding: 15px 15px 15px 0px;
+    }
+    nav a {
+        color: white;
+        padding: 15px 15px;
+        text-decoration: none;
+    }
+    nav a:hover {
+        background-color: #111;
+    }
+</style>
