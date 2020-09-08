18083c6af6b18022f4ff29591063ca3df2de1651
IngridTPires <ingrid.teixeirap@gmail.com>
2020-09-07 22:26:56 -0300

    Layout para navegação

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template lang="html">
-  <div id="app">
-    <Conteudo />
-  </div>
+    <div id="app">
+        <Navegacao />
+        <Conteudo />
+    </div>
 </template>
 
 <script>
-import Conteudo from "./components/Conteudo.vue";
-import "bootstrap/dist/css/bootstrap.min.css";
+    import Conteudo from "./components/Conteudo.vue";
+    import Navegacao from "./components/Navegacao";
+    import "bootstrap/dist/css/bootstrap.min.css";
 
-export default {
-  name: "App",
-  components: {
-    Conteudo,
-  },
-};
+    export default {
+        name: "App",
+        components: {
+            Conteudo,
+            Navegacao
+        },
+    };
 </script>
 
 <style>
-.container {
-  width: 40%;
-  margin: 20px auto 0px auto;
-}
+    .container {
+        width: 40%;
+        margin: 20px auto 0px auto;
+    }
 </style>
