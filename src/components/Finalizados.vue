7b5480b6d83d5a57caf7bc2ca91c91ff735cbe6a
IngridTPires <ingrid.teixeirap@gmail.com>
2020-09-29 17:20:12 -0300

    Dados e ajuste na exibição

--- src/components/Finalizados.vue
+++ src/components/Finalizados.vue
@@ -1,18 +1,63 @@
 <template>
     <div class="container">
         <b-list-group>
-            <b-list-group-item href="#" class="flex-column align-items-start">
+            <b-list-group-item href="#" class="flex-column align-items-start"
+                v-for="conteudo in conteudos" :key="conteudo.id">
                 <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">List group item heading</h5>
-                    <small class="text-muted">3 days ago</small>
+                    <h5 class="mb-1 text-center">{{ conteudo.titulo }}</h5>
                 </div>
 
-                <p class="mb-1">
-                    Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.
-                </p>
-
-                <small class="text-muted">Donec id elit non mi porta.</small>
+                <b-container class="bv-example-row">
+                    <b-row>
+                        <b-col>
+                            <p class="mb-1">
+                                <b>Assunto:</b> {{ conteudo.assunto }}
+                            </p>
+                            <p class="mb-1">
+                                <b>Autor:</b> {{ conteudo.autor }}
+                            </p>
+                            <p class="mb-1">
+                                <b>URL:</b> {{ conteudo.url }}
+                            </p>
+                        </b-col>
+                        <b-col>
+                            <ul>
+                                <li v-for="relacionado in conteudo.relacionados" :key="relacionado.tema">
+                                    {{ relacionado.tema }}
+                                </li>
+                            </ul>
+                        </b-col>
+                    </b-row>
+                </b-container>
             </b-list-group-item>
         </b-list-group>
     </div>
 </template>
+
+<script>
+    import firebase from "../firebaseConfig";
+
+    export default {
+        data () {
+            return {
+                conteudos: []
+            };
+        },
+        created () {
+            this.getConteudos();
+        },
+        methods: {
+            async getConteudos () {
+                firebase.conteudos.where('finalizado', '==', true).get()
+                    .then(querySnapshot => {
+                        querySnapshot.forEach(doc => {
+                            var conteudo = doc.data();
+                            conteudo.id = doc.id;
+
+                            this.conteudos.push(conteudo);
+                        })
+                    })
+            }
+        }
+    };
+</script>
