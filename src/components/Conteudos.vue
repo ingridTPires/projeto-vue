960765a600c15c9f2ddb5475eee28f8b2ab1469c
IngridTPires <ingrid.teixeirap@gmail.com>
2020-10-01 13:07:16 -0300

    Formatar data grid

--- src/components/Conteudos.vue
+++ src/components/Conteudos.vue
@@ -27,6 +27,10 @@
 
 </template>  
 
+<template v-slot:cell(finalizarAte)="row">
+    {{ new Date(row.item.finalizarAte+"T00:00:00").toLocaleDateString() }}
+</template>
+
 <template v-slot:cell(lido)="row">
     <b-form-group>
         <input type="checkbox" v-model="row.item.lido" @click.prevent="marcarLido($event, row.item.id)" :disabled="row.item.lido" />
